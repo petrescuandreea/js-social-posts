29ebaf2f1ac178e097d3d22d2d022bf070810558
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-05 14:33:33 +0100

    macro-step logici

---
+++ main.js
@@ -0,0 +1,23 @@
+/*
+
+TRACCIA:  Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+nome autore,
+foto profilo,
+data,
+testo del post,
+immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
+numero di likes.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+- Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
+
+
+*/
+
+
+// 1. creo array di oggetti 
+// 2. stampo gli elementi in pagina 
+    // 2.1 attraverso l'array => for
+    // 2.2 salvo le varie informazioni di ogni oggetto in delle variabili
+    // 2.3 stampo gli elementi in pagina => .innerHTML
+// 3. Rendiamo il tasto “Mi Piace” cliccabile => .addEventListener
